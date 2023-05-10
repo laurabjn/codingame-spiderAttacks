7ebce2ff18950147c2520a811fff768ea7c3e321
laurabjn <92058813+laurabjn@users.noreply.github.com>
2023-05-10 22:20:41 +0200

    feat: 🎸 add compare method

--- bot.cs
+++ bot.cs
@@ -1,15 +1,10 @@
 using System;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime;
 
 class Player
 {
     public const int TYPE_MONSTER = 0;
-    public const int TYPE_HERO = 1;
+    public const int TYPE_MY_HERO = 1;
     public const int TYPE_OP_HERO = 2;
 
     public class Entity
@@ -38,28 +33,118 @@ class Player
             this.NearBase = nearBase;
             this.ThreatFor = threatFor;
         }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
     }
 
     static void Main(string[] args)
     {
+
+        //Retourne la distance entre le héro et un monstre.
+        static int compareTwoDistance(int positionMonster, int positionHero)
+        {
+            positionMonster = Math.Abs(positionMonster);
+            positionHero = Math.Abs(positionHero);
+            return positionMonster - positionHero;
+        }
+
+        //Permet de savoir quelle est le monstre le plus proche de la base pour le prendre pour cible
+        static List<object> compareDistanceBetweenMonsterAndBase(Entity monster, int previousPosition, int position, int sideOfPlayer)
+        {
+            Entity target = null;
+
+            //Si aucun ennemi pris pour cible
+            if (previousPosition == 0)
+            {
+                previousPosition = monster.X + monster.Y;
+                position = monster.X + monster.Y;
+                target = monster;
+            }
+
+            //Si déja un ennemi en cible, on compare la précédente cible a la nouvelle
+            else
+            {
+                position = monster.X + monster.Y;
+
+                //Si le 2ème ennemi est plus proche du coeur de la base, on le prend pour cible
+                if ((sideOfPlayer == 0 && position < previousPosition) || (sideOfPlayer == 1 && position > previousPosition))
+                {
+                    previousPosition = position;
+                    target = monster;
+                }
+            }
+            List<object> listResult = new List<object>();
+            listResult.Add(target);
+            listResult.Add(previousPosition);
+            return listResult;
+        }
+
+        //Permet de savoir quelle est le monstre le plus proche du héros
+        static List<object> compareDistanceBetweenMonsterAndHeros(Entity monster, Entity heroes, int previousPosition, int position, int sideOfPlayer)
+        {
+            Entity target = null;
+
+            int positionHero = heroes.X + heroes.Y;
+            int positionMonster = monster.X + monster.Y;
+
+            //Si aucun ennemi pris pour cible
+            if (previousPosition == 0)
+            {
+                previousPosition = compareTwoDistance(positionMonster, positionHero);
+                position = previousPosition;
+                target = monster;
+            }
+
+            //Si déja un ennemi en cible, on compare la précédente cible a la nouvelle
+            else
+            {
+                positionBetweenMonsterAndHero = compareTwoDistance(positionMonster, positionHero);
+
+                //Si le 2ème ennemi est plus proche du héros que le 1er, on le prend pour cible
+                if ((sideOfPlayer == 0 && position < previousPosition) || (sideOfPlayer == 1 && position > previousPosition))
+                {
+                    previousPosition = position;
+                    target = monster;
+                }
+            }
+            List<object> listResult = new List<object>();
+            listResult.Add(target);
+            listResult.Add(previousPosition);
+            return listResult;
+        }
+
         string[] inputs;
         inputs = Console.ReadLine().Split(' ');
 
-        int baseX = int.Parse(inputs[0]); // The corner of the map representing your base
+        // base_x,base_y: The corner of the map representing your base
+        int baseX = int.Parse(inputs[0]);
         int baseY = int.Parse(inputs[1]);
-        
-        int heroesPerPlayer = int.Parse(Console.ReadLine()); // Always 3
+
+        // heroesPerPlayer: Always 3
+        int heroesPerPlayer = int.Parse(Console.ReadLine());
 
         // game loop
         while (true)
         {
-            for (int i = 0; i < 2; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
-                int health = int.Parse(inputs[0]); // Each player's base health
-                int mana = int.Parse(inputs[1]); // Ignore in the first league; Spend ten mana to cast a spell
-            }
+
+            inputs = Console.ReadLine().Split(' ');
+            int myHealth = int.Parse(inputs[0]); // Your base health
+            int myMana = int.Parse(inputs[1]); // Ignore in the first league; Spend ten mana to cast a spell
+
+            inputs = Console.ReadLine().Split(' ');
+            int oppHealth = int.Parse(inputs[0]);
+            int oppMana = int.Parse(inputs[1]);
+
             int entityCount = int.Parse(Console.ReadLine()); // Amount of heros and monsters you can see
+
+            List<Entity> myHeroes = new List<Entity>(entityCount);
+            List<Entity> oppHeroes = new List<Entity>(entityCount);
+            List<Entity> monsters = new List<Entity>(entityCount);
+
             for (int i = 0; i < entityCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
@@ -74,17 +159,45 @@ class Player
                 int vy = int.Parse(inputs[8]);
                 int nearBase = int.Parse(inputs[9]); // 0=monster with no target yet, 1=monster targeting a base
                 int threatFor = int.Parse(inputs[10]); // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+
+                Entity entity = new Entity(
+                    id, type, x, y, shieldLife, isControlled, health, vx, vy, nearBase, threatFor
+                );
+
+                switch (type)
+                {
+                    case TYPE_MONSTER:
+                        monsters.Add(entity);
+                        break;
+                    case TYPE_MY_HERO:
+                        myHeroes.Add(entity);
+                        break;
+                    case TYPE_OP_HERO:
+                        oppHeroes.Add(entity);
+                        break;
+                }
             }
             for (int i = 0; i < heroesPerPlayer; i++)
             {
+                Entity target = null;
 
-                // Write an action using Console.WriteLine()
-                // To debug: Console.Error.WriteLine("Debug messages...");
+                if (monsters.Count > 0)
+                {
+                    target = monsters[i % monsters.Count];
+                }
 
-
-                // In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
-                Console.WriteLine("WAIT");
+                if (target != null)
+                {
+                    Console.WriteLine($"MOVE {target.X} {target.Y}");
+                }
+                else
+                {
+                    Console.WriteLine("WAIT");
+                }
             }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
         }
     }
 }
